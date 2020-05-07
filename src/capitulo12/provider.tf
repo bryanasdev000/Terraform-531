57659223687213cfc86750288b28721c1e49016b
Bryan A. S <bryanasdev000@gmail.com>
2020-05-07 15:22:43 -0300

    Scripts do projeto final

--- src/capitulo12/provider.tf
+++ src/capitulo12/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "reliable-byte-259002"
+  project = "seu-project-id"
   region  = "southamerica-east-1"
 }
 # export GOOGLE_CLOUD_KEYFILE_JSON=path se quiserem remover a entrada de credenciais.
